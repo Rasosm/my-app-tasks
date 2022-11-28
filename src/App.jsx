8deb15c394a9ca462d753fb57a79a54e8fb5bb27
Rasosm <rasosm@gmail.com>
2022-11-28 20:50:25 +0200

    Sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css';
+// import { useState } from 'react';
+import './App.scss';
+import './button.scss';
 import Animals from './Components/Animals';
 import Betkoks from './Components/Betkoks';
 import Forest from './Components/Forest';
 import Labas from './Components/Labas';
 import Zebrai from './Components/Zebrai';
-import rand from './Functions/rand';
+
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +18,7 @@ function App() {
         <Animals dogs="buldog, smok" birds="gsacn, hdfhda"></Animals>
         <Forest forestColor="green" trees="fgjcvn, jhfj" insects="fjgf, vchh"></Forest>
         <Zebrai randColor="2"></Zebrai>
+        <button>Change shape</button>
         <Labas></Labas>
       </header>
     </div>
